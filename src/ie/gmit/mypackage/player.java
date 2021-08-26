051ebe34faa8a9a0ff3089000d18a46e7ccde3ac
Declanmoran <60182857+Declanmoran@users.noreply.github.com>
2021-08-26 03:58:09 +0100

    Create player.java

---
+++ src/ie/gmit/mypackage/player.java
@@ -0,0 +1,101 @@
+package ie.gmit.mypackage;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+
+public class Player implements Serializable {
+	
+	// Add for serialization
+		private static final long serialVersionUID = 1L;
+	
+	//Instance Variables
+		private String playerName;
+		private String club;
+		private String nationality;
+		private int age;
+		
+		//Constructor
+		public Player(String playerName) {
+			this.playerName = playerName;
+		}
+		
+		public Player(String playerName, String club, String nationality) {
+			this(playerName);
+			this.club = club;
+			this.nationality = nationality;
+		}
+
+		public Player(String playerName, String club, String nationality, int age) {
+			this(playerName, club, nationality);
+			this.age = age;
+		}
+		
+		
+		//Getters & Setters
+		public String getPlayerName(){
+			return playerName;
+		}
+		
+		public void setPlayerName(String playerName){
+			this.playerName = playerName;
+		}
+		
+		public String getclub() {
+			return club;
+		}
+
+		public void setclub(String club) {
+			this.club = club;
+		}
+
+		public String getnationality() {
+			return nationality;
+		}
+
+		public void setnationality(String nationality) {
+			this.nationality = nationality;
+		}
+
+		public int getage() {
+			return age;
+		}
+
+		public void setage(int age) {
+			this.age = age;
+		}
+
+		public String findAllFieldValuesInCSVFormat() {
+			StringBuilder listOfFields = new StringBuilder();
+			//String newLine = System.getProperty("line.separator");
+			
+			// determine fields declared in this class only (no fields of superclass)
+			Field[] fields = this.getClass().getDeclaredFields();
+
+			// print field names
+			for (Field field : fields) {
+			//	result.append("  ");
+			try {
+				// discard static fields
+				if (!java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+					//listOfFields.append(field.getName());
+					//requires access to private field:
+					listOfFields.append( field.get(this) );
+					listOfFields.append(",");
+			    }	
+					// requires access to private field:
+					//result.append(field.get(this));
+				} catch (IllegalAccessException ex) {
+					System.out.println(ex);
+				}
+				//result.append(newLine);
+			}
+			// Remove comma at end
+			if( listOfFields.length() > 0 ) {
+				listOfFields.setLength( listOfFields.length() - 1 );
+				//listOfFields.append(newLine); 
+			}
+
+			return listOfFields.toString();
+		}
+
+}
